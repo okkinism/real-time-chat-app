327e6d8c16a4a6129073776dad615f74c056e7cf
okkinism <rafa.nugraha2001@gmail.com>
2024-03-02 11:20:00 +0800

    Menambahkan perubahan yang telah diubah

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Chats = () => {
   return (
-    <div className='chats'></div>
-  )
-}
+    <div className="chats">
+      <div className="user-chat">
+        <div className="user-chat-info">
+          <img
+            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <span>Harold Christ</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chats
+export default Chats;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Search = () => {
   return (
-    <div className='search'>
-      <div className='search-form'>
-        <input type="text" placeholder='Cari Kontak' />
+    <div className="search">
+      <div className="search-form">
+        <input type="text" placeholder="Cari Kontak" />
       </div>
 
-      <div className='user-list'>
-        <div className='user-wrapper'>
-          <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+      <div className="user-chat">
+        <div className="user-chat-info">
+          <img
+            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
           <span>Harold Christ</span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
